d6a2b6d9389520f3d1881de6eed3d2b42b245dd4
Mikhail Kozhin <mishaloki@gmail.com>
2012-03-25 18:20:35 +0400

    modify app.xaml

--- App.xaml.cs
+++ App.xaml.cs
@@ -8,10 +8,7 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using Microsoft.Phone.Controls;
-using Microsoft.Phone.Shell;
+
 
 namespace BetterLifeMaker
 {
